eb6c73337a2edf3b6155a469be8de6f12d5bf805
杨德宏 <1282289444@qq.com>
2020-06-30 14:11:11 +0800

    新建项目

---
+++ Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace BlogApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// 此方法由运行时调用。使用此方法可将服务添加到容器中
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // 注册Swagger服务
+            services.AddSwaggerGen(c =>
+            {
+                // 添加文档信息
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "MyBlogApi",
+                    Version = "v1",
+                    Description = "ASP.NET CORE WebApi",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "hong3054",
+                        Email = "1282289444@qq.com"
+                    }
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        /// <summary>
+        /// 此方法由运行时调用。使用此方法配置HTTP请求管道。
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            // 启用Swagger中间件
+            app.UseSwagger();
+
+            // 配置SwaggerUI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyBlogApi");
+                c.RoutePrefix = string.Empty;
+
+            });
+
+            // 配置路由
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "api/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
